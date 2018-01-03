566a202ad97c13e3f76edfbe862483f1e28ad972
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2018-01-03 13:21:14 +0100

    running opentk window

--- GK3D/MainWindow.xaml.cs
+++ GK3D/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using GK3D.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace GK3D
 {
@@ -20,9 +23,35 @@ namespace GK3D
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Game _game;
         public MainWindow()
         {
             InitializeComponent();
+            InitWindow();
+            InitGame();
+        }
+
+        private void InitWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.Manual;
+            Left = 100;
+            Top = 100;
+            Width = 400;
+            Height = SystemParameters.PrimaryScreenHeight - 2 * Top;
+        }
+        private void InitGame()
+        {
+            var location = new System.Drawing.Point(Convert.ToInt32(Left + Width), Convert.ToInt32(Top));
+            var size = new System.Drawing.Size(Convert.ToInt32(SystemParameters.PrimaryScreenWidth - 2 * Left - Width), Convert.ToInt32(Height));
+
+            Task.Factory.StartNew(() =>
+            {
+                _game = new Game();
+                _game.Location = location;
+                _game.Size = size;
+                _game.Run(30, 30);
+            });
+            
         }
     }
 }
