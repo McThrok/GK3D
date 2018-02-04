5cf837ea3ef19f0eff3175d866e5178e0aa75c73
Unknown <wojtek.trze@gmail.com>
2018-02-04 16:58:19 +0100

    fix shader

--- GK3D/Graphics/Resources/Shaders/test/fs_primitive_color.c
+++ GK3D/Graphics/Resources/Shaders/test/fs_primitive_color.c
@@ -1,38 +1,32 @@
 ﻿#version 330
 
-
 in vec3 v_norm;
 in vec3 v_pos;
 in vec4 color;
 out vec4 outputColor;
 
-
 //light1
 uniform vec3 light_position_0;
 uniform vec3 light_color_0;
-uniform float light_ambientIntensity_0;
-uniform float light_diffuseIntensity_0;
-
-uniform vec3 material_ambient;
-uniform vec3 material_diffuse;
-uniform vec3 material_specular;
-uniform float material_specExponent;
 
 uniform mat4 view;
 
-vec4 getColor(vec3 light_position, vec3 light_color, float light_ambientIntensity, float light_diffuseIntensity) {
+vec4 getColor(vec3 light_position, vec3 light_color) {
+
+
 	vec3 lightColor = light_color;
 	vec3 lightPos = light_position;
 	vec3 FragPos = v_pos;
 
 	// ambient
-	vec3 ambient = light_ambientIntensity * lightColor;
+	float ambientStrength = 0.2f;
+	vec3 ambient = ambientStrength * lightColor;
 
 	// diffuse 
 	vec3 norm = normalize(v_norm);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = light_diffuseIntensity * diff * lightColor;
+	vec3 diffuse = diff * lightColor;
 
 	// specular
     float specularStrength = 0.2f;
@@ -49,5 +43,5 @@ vec4 getColor(vec3 light_position, vec3 light_color, float light_ambientIntensit
 void main()
 {
 	outputColor = vec4(0, 0, 0, 1);
-	outputColor += getColor(light_position_0, light_color_0, light_ambientIntensity_0, light_diffuseIntensity_0);
+	outputColor += getColor(light_position_0, light_color_0);
 }
